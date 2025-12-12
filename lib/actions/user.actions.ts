785ede8a97d552c3051dc4e2493815c4ddd7389f
vedantj77 <vedantjadhav575@gmail.com>
2025-12-13 02:13:33 +0530

    Authentication  commit

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,34 @@
+'use server';
+
+import {connectToDatabase} from "@/database/mongoose";
+
+// ADD THIS INTERFACE
+export interface UserForNewsEmail {
+  id: string;
+  email: string;
+  name: string;
+  country?: string;
+}
+
+export const getAllUsersForNewsEmail = async (): Promise<UserForNewsEmail[]> => {
+    try {
+        const mongoose = await connectToDatabase();
+        const db = mongoose.connection.db;
+        if(!db) throw new Error('Mongoose connection not connected');
+
+        const users = await db.collection('user').find(
+            { email: { $exists: true, $ne: null }},
+            { projection: { _id: 1, id: 1, email: 1, name: 1, country:1 }}
+        ).toArray();
+
+        return users.filter((user) => user.email && user.name).map((user) => ({
+            id: user.id || user._id?.toString() || '',
+            email: user.email,
+            name: user.name,
+            country: user.country  // Optional
+        }))
+    } catch (e) {
+        console.error('Error fetching users for news email:', e)
+        return []
+    }
+}
---
+++ lib/inngest/client.ts
@@ -0,0 +1,7 @@
+// lib/inngest.ts
+import { Inngest } from "inngest";
+
+export const inngest = new Inngest({
+    id: 'signalist',
+    ai: { gemini: { apiKey: process.env.GEMINI_API_KEY! }}
+})
